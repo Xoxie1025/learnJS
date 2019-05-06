0930f16847d4bd0c48c99e43b7135465b9f9bc19
Xoxie <xoxie@yandex.ru>
2019-05-06 19:42:15 +0300

    Добавленно ДЗ 1

--- hw1/js/script.js
+++ hw1/js/script.js
@@ -2,6 +2,4 @@ let money = prompt("Ваш бюджет на месяц?", 0);
 let time = prompt("Введите дату в формате YYYY-MM-DD", "Сегодня");
 console.log("Ваш бюджет на " + time + " - " + money + " руб.");
 
-let appData = {
-    
-}
+let
